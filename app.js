9ae2887bb22b7860b7483e85373cb98f8c2ed0a8
Rafael Coelho <rafacoelhoas@gmail.com>
2023-03-11 21:01:16 -0300

    Proteção de Rotas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();;
 const express = require('express');
 const app = express();
 const useRouter = require('./routes/userRouter')
+const adminRouter = require('./routes/adminRouter')
 const mongoose =require('mongoose');
 
 mongoose.set("strictQuery", false);
@@ -18,5 +19,7 @@ mongoose.connect(process.env.MONGO_CONNECTION_URL,
 
 
 app.use('/user', express.json(), useRouter);
+
+app.use('/admin', express.json(), adminRouter)
 
 app.listen(process.env.PORT, () =>{console.log("Server Running ")})
--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const userSchema = mongoose.Schema({
     name: { type: String, required: true, min: 3, max: 50 },
     email: { type: String, required: true, min: 3, max: 70 },
     password: { type: String, required: true, min: 6, max: 50 },
-    createAt: { type: Date, default: Date.now }
+    createAt: { type: Date, default: Date.now },
+    admin:{type: Boolean, default: false}
 })
 
 module.exports = mongoose.model('User', userSchema);
